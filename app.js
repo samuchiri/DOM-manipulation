5e74ee693cf3eb9928bd02edb88fe8ca41bd0234
samuchiri <s.muchiri.999@gmail.com>
2023-07-28 09:08:54 +0300

    Parent and Child

---
+++ app.js
@@ -0,0 +1,17 @@
+function myWorkingFunction(){
+    // alert("This is a drill. Rudi nyumbani!!!");
+    document.getElementById('oldText').innerHTML='NEW TEXT BANA!!!';
+}
+
+const image_input=document.querySelector('#image_input');
+var uploadedImage ="";
+image_input.addEventListener('change', readerFunction());
+
+function readerFunction(){
+    const reader=new FileReader();
+    reader.addEventListener("load", ()=>{
+        uploaded_image=reader.result;
+        document.querySelector("#display_image").style.backgroundImage=`url=(${uploaded_image})`
+    });
+    reader.readAsDataURL(this.files[0]);
+}
---
+++ app1.js
@@ -0,0 +1,7 @@
+const ul=document.querySelector('ul');
+ const liLearn=createElement('li');
+ ul.appendChild('liLearn');
+ liLearn.textContent="Learn Javascript";
+const liMaster=document.createElement('li');
+ul.appendChild('liMaster');
+liMaster.textContent="Master Javascript";
